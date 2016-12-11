853dbc52a758b43d316f375fdab439312d3c3b1e
shin039 <life.syncopation.4003@gmail.com>
2016-12-11 22:13:53 +0900

    Testクラスの作成

--- src/practice_java/Instant/HelloWorld.java
+++ src/practice_java/Instant/HelloWorld.java
@@ -1,8 +1,22 @@
 package practice_java.Instant;
 
+/**
+ * HelloWorld
+ */
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+  /**
+   * Main関数
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println(helloworld());
+  }
+
+  /**
+   * HelloWorld返却クラス
+   */
+  public static String helloworld(){
+    return "Hello World!";
+  }
 }
---
+++ src/practice_java/InstantTest/HelloWorld_Test.java
@@ -0,0 +1,63 @@
+package practice_java.InstantTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import practice_java.Instant.HelloWorld;
+
+/**
+ * HelloWorld.javaのテストクラス
+ * @author Shin
+ *
+ */
+public class HelloWorld_Test {
+
+  /**
+   * テストクラスのstaticイニシャライザ後に呼ばれる。
+   */
+  @BeforeClass
+  public static void BeforeClass(){
+
+  }
+
+  /**
+   * テストクラスのコンストラクタ後
+   */
+  @Before
+  public void before(){
+
+  }
+
+  /**
+   * テストメソッド
+   */
+  @Test
+  public void Test(){
+    String expect_return = "Hello World!";
+    String check_target = HelloWorld.helloworld();
+
+    assertEquals(expect_return, check_target);
+  }
+
+  /**
+   * テストメソッド実行後
+   */
+  @After
+  public void After(){
+
+  }
+
+
+  /**
+   * テストクラスの実行後
+   */
+  @AfterClass
+  public static void AfterClass(){
+
+  }
+}
